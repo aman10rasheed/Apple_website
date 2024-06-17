bef9e767719470e35773c428c21010a387c06068
amanrasheed <amanrasheed@cygnonex.com>
2024-06-17 23:49:31 +0530

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <main className="bg-black"></main>;
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 }
 
 export default App;
